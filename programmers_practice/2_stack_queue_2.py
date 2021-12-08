38017e1c5e6995098bd6d3ea4e1e0973ef6eddfc
msio900 <msio900@gmail.com>
2021-12-08 14:28:27 +0900

    1208 3rd commit

---
+++ programmers_practice/2_stack_queue_2.py
@@ -0,0 +1,9 @@
+def solution(priorities, location):
+    answer = 0
+    return answer
+
+
+if __name__ == '__main__':
+    priorities = [2, 1, 3, 2]
+    location = 2
+    print(solution(priorities, location))

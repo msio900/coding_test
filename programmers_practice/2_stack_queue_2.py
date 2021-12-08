17c1757c8f825ebecf5b06e504377c210b559f88
msio900 <msio900@gmail.com>
2021-12-08 17:10:13 +0900

    1208 7th commit

--- programmers_practice/2_stack_queue_2.py
+++ programmers_practice/2_stack_queue_2.py
@@ -1,26 +1,14 @@
 def solution(priorities, location):
     answer = 0
-    w_list = [i for i in range(len(priorities))]
-    print(w_list)
-    print(priorities)
+    w_list = list(range(len(priorities)))
     J = w_list[location]
-    print('j는',J)
-    point_idxs = []
 
     for i in range(len(priorities)):
         for j in range(i+1,len(priorities)):
             print('i : ',i,'j :',j)
             if priorities[i] < priorities[j]:
-                point_idxs.append(i)
-                break
-    for point_idx in point_idxs:
-        w_list.append(w_list[point_idx])
-        print(w_list)
-    for point_idx in point_idxs:
-        w_list.pop(0)
-        print(w_list)
+                print(priorities[i],":", priorities[j])
 
-    
     answer = w_list.index(J)+1
         
 

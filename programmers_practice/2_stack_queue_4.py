8afe7ccef0ac5988eeeecbd51e9c2261f23f82e4
msio900 <msio900@gmail.com>
2021-12-10 14:43:21 +0900

    1210 3rd commit

--- programmers_practice/2_stack_queue_4.py
+++ programmers_practice/2_stack_queue_4.py
@@ -2,11 +2,13 @@ def solution(prices):
     answer = []
     for i in range(0, len(prices)):
         for j in range(i+1, len(prices)):
-            if prices[i] > prices[j]:
+            if j == (len(prices)-1) :
+                answer.append(j-i)
+            elif prices[i] >prices[j]:
                 answer.append(j-i)
                 break
-        if j == (len(prices)-1):
-            answer.append(j-i)
+    answer.append(0)
+            
     return answer
 
 

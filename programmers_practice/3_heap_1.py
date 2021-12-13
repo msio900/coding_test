efa974bd25207ecdc9ec72fdcea02774e967616f
msio900 <msio900@gmail.com>
2021-12-13 23:16:28 +0900

    1213 5th commit

--- programmers_practice/3_heap_1.py
+++ programmers_practice/3_heap_1.py
@@ -1,12 +1,31 @@
+import heapq
+
 def solution(scoville, K):
     answer = 0
-
-    print(scoville.sorted())
+    print(scoville)
+    heapq.heapify(scoville)
+    print(scoville)
+    # scoville.sort()
+    # if scoville[-1] == 0:
+    #     print(f'스코빌지수{K}를 만들 수 없음.')
+    #     answer = -1
+    # for i in range(len(scoville)-1):
+    #     answer += 1
+    #     scoville.insert(0, scoville.pop(0)+scoville.pop(0)*2)
+    #     print(f'{answer}회차, 정렬 전{scoville}')
+    #     scoville.sort()
+    #     print(f'{answer}회차, 정렬 후{scoville}')
+    #     if scoville[0] >= K:
+    #         print('끝')
+    #         break
+    # if scoville[0] < K:
+    #     print(f'스코빌지수{K}를 만들 수 없음.')
+    #     answer = -1
 
     return answer
 
 
 if __name__ == '__main__':
     scoville = [1, 2, 3, 9, 10, 12]
-    K = 2
+    K = 110
     print(solution(scoville, K))

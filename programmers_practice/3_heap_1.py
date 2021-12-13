75a6be256618d1b5c54c3f53e6bed6d2fb45f057
msio900 <msio900@gmail.com>
2021-12-13 10:35:40 +0900

    1213 3rd commit

--- programmers_practice/3_heap_1.py
+++ programmers_practice/3_heap_1.py
@@ -1,10 +1,12 @@
 def solution(scoville, K):
     answer = 0
+
+    print(scoville.sorted())
+
     return answer
 
 
 if __name__ == '__main__':
     scoville = [1, 2, 3, 9, 10, 12]
     K = 2
-
-    print(solution(prices))
+    print(solution(scoville, K))

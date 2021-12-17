b6364aadfb4885c0d05a057ca2216702c064d8d5
msio900 <msio900@gmail.com>
2021-12-17 18:38:06 +0900

    1217 6th commit

--- acmicpc/1935/1935.py
+++ acmicpc/1935/1935.py
@@ -1,14 +1,16 @@
 import sys
 
-
-
 N = int(sys.stdin.readline())
 sentence = sys.stdin.readline().strip()
 
 stack = []
 operator = ['+','-','/','*']
+o_list = []
 
 for i in sentence:
     if i in operator:
-        stack.append(i)
+        o_list.append(i)
+for _ in range(N):
+    stack.append(int(sys.stdin.readline()))
+print(o_list)
 print(stack)

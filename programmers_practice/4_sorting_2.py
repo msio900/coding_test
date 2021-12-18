376a06302e0339bb3f9b5adaa50ca635886e7598
msio900 <msio900@gmail.com>
2021-12-18 22:00:45 +0900

    1218 3rd commit

--- programmers_practice/4_sorting_2.py
+++ programmers_practice/4_sorting_2.py
@@ -1,6 +1,6 @@
 def solution(numbers):
-    answer = ''
-    return answer
+    numbers.sort(key = lambda x : str(x) * 3, reverse = True)
+    return str(int("".join(list(map(str, numbers)))))
 
 
 if __name__ == '__main__':

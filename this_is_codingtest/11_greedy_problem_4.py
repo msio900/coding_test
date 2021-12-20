2a27d5edcc990c4e12b5498473aee5f06367c05f
msio900 <msio900@gmail.com>
2021-12-20 14:56:06 +0900

    1220 6th commit

---
+++ this_is_codingtest/11_greedy_problem_4.py
@@ -0,0 +1,27 @@
+import sys
+
+N = int(sys.stdin.readline())
+coins = sys.stdin.readline().split()
+
+coins = [int(i) for i in coins]
+
+print(coins)
+coins.sort()
+print(coins)
+
+stack = []
+for i in range(len(coins)):
+ for j in range(i+1, len(coins)+1):
+  stack.append(sum(coins[i:j]))
+print(list(set(stack)))
+
+not_list = [i for i in range(1, max(stack))]
+print(not_list)
+
+for i in stack:
+ if i in not_list:
+  not_list.remove(i)
+
+not_list.sort()
+
+print(not_list[0])

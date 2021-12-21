166ea57e3d82c2b7976c1f4ebc042d8f80ad42ac
msio900 <msio900@gmail.com>
2021-12-21 20:07:40 +0900

    1221 3rd commit

--- acmicpc/10808/10808.py
+++ acmicpc/10808/10808.py
@@ -8,12 +8,9 @@ alphabet = [chr(i) for i in range(97, 123)]
 
 X = Counter(alphabet)+Counter(S)
 X = dict(X)
-
+# 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
 answer = []
 for i in X:
     answer.append(str(X[i] - 1))
 
 print(" ".join(answer))
-
-
-## 

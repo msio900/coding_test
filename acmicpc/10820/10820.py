3ac764f9ad763b920c035e350dd3046f1ac0c8d4
msio900 <msio900@gmail.com>
2021-12-23 09:37:47 +0900

    1223 2nd commit

--- acmicpc/10820/10820.py
+++ acmicpc/10820/10820.py
@@ -1,15 +1,21 @@
 import sys
 
-S = list(map(str, sys.stdin.readline().strip()))
+# |   소문자   |   대문자   |   숫자   |   공백   |의 갯수
 
-alphabet = []
+while True:
+    sentence = sys.stdin.readline()
+    a, b, c, d = 0, 0, 0, 0
+    print(sentence)
 
-for i in range(ord('a'), ord('z')+1):
-    for j in range(len(S)):
-        if S[j] == chr(i):
-            print(f'{chr(i)}는 {j}번째,  {S[j]} 등장 ')
-            alphabet.append(j)
-            break
-        else:
-            alphabet.append(-1)
-print(alphabet)
+    if not sentence:
+        break
+    for i in sentence:
+        if i.islower():
+            a += 1
+        elif i.isupper():
+            b += 1
+        elif ord('0') <= ord(i) <= ord('9'):
+            c += 1
+        elif i == ' ':
+            d += 1
+    print(a, b, c, d)

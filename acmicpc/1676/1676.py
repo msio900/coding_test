4e76651fb4d33b0d6bf6b9a4b59ec0ffb0df03eb
msio900 <msio900@gmail.com>
2022-01-10 13:21:20 +0900

    0110 2nd commit

--- acmicpc/1676/1676.py
+++ acmicpc/1676/1676.py
@@ -1,10 +1,17 @@
 import sys
+import math
+from functools import reduce
+# 10! = 10*9*8*...*1
 
-while True:
-    n = int(sys.stdin.readline())
-    for i in range(2,n):
-        if
+answer = 0
+N = int(sys.stdin.readline())
 
-
-    if not n:
+for i in reversed(str(math.factorial(N))):
+    if i == '0':
+        answer += 1
+    else:
         break
+
+print(answer)
+
+

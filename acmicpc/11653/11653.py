811626bd1b655ae715689614d9493af60b17eb65
msio900 <msio900@gmail.com>
2022-01-31 20:11:01 +0900

    0131 2nd commit

--- acmicpc/11653/11653.py
+++ acmicpc/11653/11653.py
@@ -1,21 +1,10 @@
 import sys
 
-A, B = map(int, sys.stdin.readline().split())
-m = int(sys.stdin.readline())
-# print(A, B)
-# print(m)
+N = int(sys.stdin.readline())
 
-A_nums = list(map(int, sys.stdin.readline().split()))
-
-# print(A_nums)
-A_num = 0
-# 10진법으로 변환
-for i in range(m):
-    A_num += A_nums[i]*A**((m-1)-i)
-
-B_nums = []
-while A_num != 0:
-    B_nums.append(str(A_num % B))
-    A_num = A_num // B
-
-print(' '.join(B_nums[::-1]))
+while N != 1:
+    for i in range(2, N+1):
+        if N % i == 0:
+            print(i)
+            N = N // i
+            break

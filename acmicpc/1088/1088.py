d3844749ad1406a2a4cfcc95911d7c4875dc640f
msio900 <msio900@gmail.com>
2022-03-31 17:33:12 +0900

    0331 1st commit

--- acmicpc/1088/1088.py
+++ acmicpc/1088/1088.py
@@ -1,14 +1,16 @@
 import sys
 
+# in[0]
+# 2
+# 1 3
+# 1
+# out[0]
+# 0.5
 
 N = int(sys.stdin.readline())
-
-d = [0] * (N + 1)
-p = [0] + list(map(int, sys.stdin.readline().split()))
-d[1] = p[1]
-
-for i in range(2, N + 1):
-    for j in range(1, i + 1):
-        if d[i] < d[i - j] + p[j]:
-            d[i] = d[i - j] + p[j]
-print(d[N])
+cake_gram = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+print(cake_gram)
+for _ in range(M):
+    cake_gram.sort(reverse=True)
+    cake_gram.append(cake_gram.pop()/2)

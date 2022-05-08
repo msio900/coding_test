0c951077544660a095893c4d674c7fc5a63086f9
msio900 <msio900@gmail.com>
2022-05-09 08:40:07 +0900

    0509 1st commit

---
+++ acmicpc/re_1874/re_1874.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    x = input().rstrip()
+    cnt = 0
+    com = 0
+    for i in x:
+        if i == '(':
+            cnt += 1
+        elif i == ')':
+            cnt -= 1
+            if cnt < 0:
+                break
+
+    print('YES') if not cnt else print('NO')
+
+

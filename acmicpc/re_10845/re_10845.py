46cec19eba95b3a21e9bab6e2625eaa601469aee
msio900 <msio900@gmail.com>
2022-05-11 09:56:48 +0900

    0511 2nd commit

--- acmicpc/re_10845/re_10845.py
+++ acmicpc/re_10845/re_10845.py
@@ -1,1 +1,33 @@
-imp
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+que = []
+for _ in range(N):
+    order = input().split()
+    if order[0] == 'push':
+        que.append(order[1])
+    if order[0] == 'pop':
+        if que:
+            print(que.pop(0))
+        else:
+            print(-1)
+    if order[0] == 'size':
+        print(len(que))
+    if order[0] == 'empty':
+        if que:
+            print(0)
+        else:
+            print(1)
+    if order[0] == 'front':
+        if que:
+            print(que[0])
+        else:
+            print(-1)
+    if order[0] == 'back':
+        if que:
+            print(que[-1])
+        else:
+            print(-1)

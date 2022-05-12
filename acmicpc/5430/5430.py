08986962cf0333e48ec55ee59368d6f43f55b1f6
msio900 <msio900@gmail.com>
2022-05-12 11:38:10 +0900

    0512 2nd commit

--- acmicpc/5430/5430.py
+++ acmicpc/5430/5430.py
@@ -1,20 +1,16 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-
-n, k = map(int, input().rstrip().split())
-
-que = deque()
-
-for i in range(1, n+1):
-    que.append(i)
-answer = []
-
-while que:
-    for i in range(k-1):
-        que.append(que.popleft())
-    answer.append(str(que.popleft()))
-
-answer = ', '.join(answer)
-print(f"<{answer}>")
+# import sys
+#
+# input = sys.stdin.readline
+#
+# # R : 함수 R은 배열에 있는 수의 순서를 뒤집는 함수
+# # D : 첫 번째 수를 버리는 함수
+# # 열이 비어있는데 D를 사용한 경우에는 에러가 발생
+#
+# # 테스트 케이스의 수
+# T = int(input())
+#
+# for _ in range(T):
+#     function = input().rstrip()
+#     n = int(input())
+#     array = input().rstrip()
+#     continue

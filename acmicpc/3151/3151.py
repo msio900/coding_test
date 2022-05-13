1435eec6e43bfa14159d0342b204c7b676e7882b
msio900 <msio900@gmail.com>
2022-05-13 21:12:57 +0900

    0513 2nd commit

--- acmicpc/3151/3151.py
+++ acmicpc/3151/3151.py
@@ -2,29 +2,18 @@
 # -10000 < Aj < 10000
 # 합이 0이 되는 팀을 만들고자 함
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 N = int(input())
 
 A_list = list(map(int, input().split()))
+
 cnt = 0
-print(A_list)
+for i in combinations(A_list, 3):
+    # print(i)
+    if sum(i) == 0:
+        cnt += 1
 
-
-# 이진 검색
-List = []
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
-    for k, v in enumerate(numbers):
-        left, right = k + 1, len(numbers) - 1
-        expected = target - v
-        while left <= right:
-            mid = left + (right - left) //2
-            if numbers[mid] < expected:
-                left = mid +1
-            elif numbers[mid] > expected:
-                right = mid -1
-            else:
-                return k + 1, mid + 1
-
-print(twoSum(A_list, cnt))
+print(cnt)

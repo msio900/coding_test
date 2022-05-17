bcec405ed016b47beddec26379cda41593fd4710
msio900 <msio900@gmail.com>
2022-05-17 18:02:16 +0900

    0517 2nd commit

--- acmicpc/1149/1149.py
+++ acmicpc/1149/1149.py
@@ -1,10 +1,19 @@
+# 1번 집부터 n번 집까지 집이 순서대로 있음
+# R G B 중 하나의 색으로 집을 칠해야함.
+# 1번 집의 색은 2번 집의 색과 같지 않아야 함.
+# n번 집의 색은 n-1번 집의 색과 같지 않아야함.
+# i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
 import sys
 
 input = sys.stdin.readline
 
-str_arr = input()
-bomb = input()
-while bomb in str_arr:
-    print(str_arr, f'{bomb}')
-    str_arr = str_arr.strip(bomb)
-    print(str_arr)
+n = int(input())
+answer = 0
+for _ in range(n):
+    costs = list(map(int, input().split()))
+    print(costs, costs.index(min(costs)))
+    k = costs.index(min(costs))
+    answer += min(costs)
+
+print(answer)
--- acmicpc/9935/9935.py
+++ acmicpc/9935/9935.py
@@ -1,19 +1,21 @@
-# 1번 집부터 n번 집까지 집이 순서대로 있음
-# R G B 중 하나의 색으로 집을 칠해야함.
-# 1번 집의 색은 2번 집의 색과 같지 않아야 함.
-# n번 집의 색은 n-1번 집의 색과 같지 않아야함.
-# i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
-
 import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-answer = 0
-for _ in range(n):
-    costs = list(map(int, input().split()))
-    print(costs, costs.index(min(costs)))
-    k = costs.index(min(costs))
-    answer += min(costs)
+str_arr = input().rstrip()
+bomb = list(input().rstrip())
 
-print(answer)
+
+# print(1, str_arr)
+stack = []
+
+for i in range(len(str_arr)):
+    stack.append(str_arr[i])
+
+    if len(stack) >= len(bomb) and stack[-len(bomb):] == bomb:
+        del stack[-len(bomb):]
+
+if stack:
+    print(''.join(stack))
+else:
+    print('FRULA')

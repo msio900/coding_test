1bca206ef81f8e5edf779a7a36d6a343d3a81596
msio900 <msio900@gmail.com>
2022-05-18 09:45:14 +0900

    0518 1st commit

---
+++ acmicpc/re_17298/re_17298.py
@@ -0,0 +1,33 @@
+# import sys
+#
+# input = sys.stdin.readline
+#
+# N = int(input())
+# nums = list(map(int, input().split()))
+
+## 이중 반복문 - 시간초과
+# for i in range(len(nums)-1):
+#     j = i+1
+#     point = True
+#     while j < len(nums):
+#         if nums[i] < nums[j]:
+#             print(nums[j], end=' ')
+#             point = False
+#             break
+#         j += 1
+#     if point:
+#         print(-1, end=' ')
+#
+#
+# print(-1)
+
+# 스택을 이용한 풀이
+answer = [-1]*N
+stack = []
+stack.append(0)
+for i, j in enumerate(nums):
+    while stack and nums[stack[-1]] < nums[i]:
+        answer[stack.pop()] = nums[i]
+    stack.append(i)
+
+print(*answer)

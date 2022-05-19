271e5111d2fc4286327c0c739213e82a317d71ec
msio900 <msio900@gmail.com>
2022-05-19 10:03:22 +0900

    0519 2nd commit

--- acmicpc/re_17299/re_17299.py
+++ acmicpc/re_17299/re_17299.py
@@ -1,11 +1,22 @@
 import sys
+from collections import Counter
+input = sys.stdin.readline
 
-input = str(sys.stdin.readline().strip())
-cut = 0
-for i in range(len(input)-1):
-    if (input[i] == '(') & (input[i+1] == ')'):
-        cut += 1
-        print(f'절단 {cut}회')
-    elif (input[i] == '(') & (input[i+1] == '('):
-        cut += 1
-        print(f'절단 {cut}회')
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+
+arr_cnt = Counter(arr)
+# print(arr_cnt)
+
+answer = [-1]*N
+stack = []
+
+stack.append(0)
+for i in range(1, N):
+    while stack and arr_cnt[arr[stack[-1]]] < arr_cnt[arr[i]]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*answer)

b62d16183ae7b5094ec73b225103f31f9fd8086d
msio900 <msio900@gmail.com>
2022-05-23 08:57:56 +0900

    0523 1st commit

--- acmicpc/re_10808/re_10808.py
+++ acmicpc/re_10808/re_10808.py
@@ -1,16 +1,16 @@
 import sys
 from collections import Counter
 
-S = sys.stdin.readline().strip()
-S = [str(i) for i in S]
+input = sys.stdin.readline
 
-alphabet = [chr(i) for i in range(97, 123)]
+str_arr = input().rstrip()
 
-X = Counter(alphabet)+Counter(S)
-X = dict(X)
-# 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
-answer = []
-for i in X:
-    answer.append(str(X[i] - 1))
+str_arr_cnt = Counter(str_arr)
 
-print(" ".join(answer))
+answer = [0 for i in range(97,123)]
+# print(answer)
+# print(str_arr_cnt)
+for k in str_arr_cnt:
+    answer[ord(k)-97] = str_arr_cnt.get(k)
+
+print(*answer)

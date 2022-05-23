92c551c23ca75dfa3e55dcd0864878a19651ac7e
msio900 <msio900@gmail.com>
2022-05-23 09:39:21 +0900

    0523 1st commit

--- acmicpc/re_1918/re_1918.py
+++ acmicpc/re_1918/re_1918.py
@@ -3,4 +3,29 @@ import sys
 input = sys.stdin.readline
 exp = input().rstrip()
 
-print(exp)
+answer = ''
+opr_list = []
+for i in exp:
+    if i.isalpha():
+        answer += i
+    else:
+        if i =='(':
+            opr_list.append(i)
+        elif i == '*' or i == '/':
+            while opr_list and (opr_list[-1] == '*' or opr_list[-1] == '/'):
+                answer += opr_list.pop()
+            opr_list.append(i)
+        elif i == '+' or i == '-':
+            while opr_list and opr_list[-1] != '(':
+                answer += opr_list.pop()
+            opr_list.append(i)
+        elif i == ')':
+            while opr_list and opr_list[-1] != '(':
+                answer += opr_list.pop()
+            opr_list.pop()
+
+while opr_list:
+    answer += opr_list.pop()
+
+
+print(answer)

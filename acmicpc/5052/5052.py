b318540e87c05c025900fa95b098e08618ae0619
msio900 <msio900@gmail.com>
2022-05-24 10:11:10 +0900

    0524 2nd commit

--- acmicpc/5052/5052.py
+++ acmicpc/5052/5052.py
@@ -2,20 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-# t : 테스트 케이스
 t = int(input())
 
 for _ in range(t):
     # n : 전화번호의 수
     n = int(input())
     num_list = [input().rstrip() for i in range(n)]
+    num_list.sort()
     # print(num_list)
-    bool = True
-    for i in range(n):
-        for j in range(i + 1, n):
-            if num_list[i] == num_list[j][:len(num_list[i])]:
-                bool = False
-                print('NO')
-                break
-    if bool:
+    for i in range(n-1):
+        if num_list[i] == num_list[i+1][:len(num_list[i])]:
+            print('NO')
+            break
+    else :
         print('YES')

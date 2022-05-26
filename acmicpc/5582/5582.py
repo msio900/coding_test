622899d38dad361de0b2620b2b98839275711aff
msio900 <msio900@gmail.com>
2022-05-27 08:47:57 +0900

    0527 1st commit

--- acmicpc/5582/5582.py
+++ acmicpc/5582/5582.py
@@ -1,1 +1,15 @@
+import sys
+input = sys.stdin.readline
 
+S1 = input().rstrip()
+S2 = input().rstrip()
+answer = 0
+
+dp = [[0]*(len(S2)+1) for _ in range(len(S1)+1)]
+
+for i in range(1, len(S1)+1):
+    for j in range(1, len(S2) + 1):
+        if (S1[i-1] == S2[j-1]):
+            dp[i][j] = dp[i-1][j-1]+1
+            answer = max(dp[i][j], answer)
+print(answer)
---
+++ acmicpc/re_10824/re_10824.py
@@ -0,0 +1,6 @@
+import sys
+
+input = sys.stdin.readline
+A,B,C,D = input().split()
+
+print(int(A+B)+int(C+D))

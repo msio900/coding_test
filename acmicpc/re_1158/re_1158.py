4a9ab1122f23332c19cdf46b8e5d2f095b731ae1
msio900 <msio900@gmail.com>
2022-05-28 09:07:59 +0900

    0528 1st commit

--- acmicpc/re_1158/re_1158.py
+++ acmicpc/re_1158/re_1158.py
@@ -1,20 +1,41 @@
+# import sys
+# from collections import deque
+#
+# input = sys.stdin.readline
+#
+# n, k = map(int, input().rstrip().split())
+#
+# que = deque()
+#
+# for i in range(1, n+1):
+#     que.append(i)
+# answer = []
+#
+# while que:
+#     for i in range(k-1):
+#         que.append(que.popleft())
+#     answer.append(str(que.popleft()))
+#
+# answer = ', '.join(answer)
+# print(f"<{answer}>")
+
+
+
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 n, k = map(int, input().rstrip().split())
 
-que = deque()
 
-for i in range(1, n+1):
-    que.append(i)
-answer = []
+circle_list = [i for i in range(1, n+1)]
+remove_list = []
 
-while que:
-    for i in range(k-1):
-        que.append(que.popleft())
-    answer.append(str(que.popleft()))
+idx = 0
+for i in range(n):
+    idx += k - 1
+    if idx >= len(circle_list):
+        idx = idx % len(circle_list)
+    remove_list.append(str(circle_list.pop(idx)))
 
-answer = ', '.join(answer)
-print(f"<{answer}>")
+print(f'<{", ".join(remove_list)}>')

e0e6aa0de82a0f51876528a2cc2778fcb7b5823e
msio900 <msio900@gmail.com>
2022-05-28 09:02:57 +0900

    0527 4th commit

--- programmers_practice/programmers_92343.py
+++ programmers_practice/programmers_92343.py
@@ -1,28 +1,42 @@
-# n : 양의 정수
-# k : 바꿀 진수
-# 아래 조건에 맞는 소수가 몇개인지?
-# 0P0처럼 소수 양쪽에 0이 있는 경우
-# P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
-# 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
-# P처럼 소수 양쪽에 아무것도 없는 경우
-# 단, P는 각 자릿수에 0을 포함하지 않는 소수입니다.
-# 예를 들어, 101은 P가 될 수 없습니다.
+from collections import defaultdict, deque
+from copy import deepcopy
+
+is_wolf = list()
+num2edges = defaultdict(list)
+max_sheep = 0
 
 
+def find_max_recursive(current_loc, used, nsheep, nwolf, can_go):
+    global max_sheep
 
-def solution(n, k):
-    # n을 k 진수로 바꾸기
-    k_list = []
-    while n != 0:
-        k_list.append(n % k)
-        n //= k
-        print(k_list, n)
-    answer = -1
+    if used[current_loc]: return  # 현재 노드를 방문한 경우 return
+    used[current_loc] = True  # 방문 기록
 
-    return answer
+    if is_wolf[current_loc]:  # 늑대인 경우 늑대 count += 1
+        nwolf += 1
+    else:
+        nsheep += 1  # 양인 경우 양 count += 1
+        max_sheep = max(max_sheep, nsheep)  # 양 최대 수 갱신
+    if nsheep <= nwolf: return  # 늑대 수가 양의 수와 같거나 많은 경우 return
 
-if __name__ == "__main__":
-    n, k = 437674, 3
-    print('1st Test Case', solution(n, k))
-    n, k = 110011, 10
-    print('2nd Test Case', solution(n, k))
+    can_go.extend(num2edges[current_loc])  # 현재 노드와 연결된 노드를 추가함
+    for next_loc in can_go:
+        # q에 저장된 노드서 하나를 가져와서 재귀함수 요청
+        # 이 때 다음 q에는 현재 노드를 제외한 리스트로 재구성하여 재귀함수 요청
+        find_max_recursive(next_loc, deepcopy(used), nsheep, nwolf,
+                           can_go=[loc for loc in can_go if loc != next_loc and not used[loc]])
+
+
+def solution(info, edges):
+    global is_wolf, num2edges, max_sheep
+    is_wolf = info  # 전역변수에 할당
+    used = [False] * len(is_wolf)  # 방문한 노드 확인 용 변수
+
+    for e_from, e_to in edges:
+        num2edges[e_from].append(e_to)  # 연결된 엣지 리스트에 추가
+    print(num2edges)
+    for i, v in num2edges.items():
+        print(i, ":", v)
+    # start
+    find_max_recursive(0, used, 0, 0, [])
+    return max_sheep

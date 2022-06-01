8cb82a880dc4a486a796da10de3264237e9913af
msio900 <msio900@gmail.com>
2022-06-01 10:04:22 +0900

    0601 2nd commit

--- acmicpc/11478/11478.py
+++ acmicpc/11478/11478.py
@@ -1,39 +1,11 @@
-# import sys
-#
-# input = sys.stdin.readline
-#
-# M, N = map(int, input().split())
-#
-# def isPrime(a):
-#   if(a<2):
-#     return False
-#   for i in range(2,a):
-#     if(a%i==0):
-#       return False
-#   return True
-#
-# for i in range(M, N + 1):
-#   if(isPrime(i)):
-#     print(i)
-
-
 import sys
 
 input = sys.stdin.readline
 
-M, N = map(int, input().split())
-
-a = [False,False] + [True]*(N-1)
-primes=[]
-
-for i in range(2, N+1):
-    if a[i]:
-        primes.append(i)
-    for j in range(2*i, N+1, i):
-        a[j] = False
-
-# print(primes)
-
-for i in primes:
-    if M <= i <=N:
-        print(i)
+S = input().rstrip()
+ans = []
+for i in range(len(S)+1):
+    for j in range(i + 1, len(S)+1):
+        print(S[i:j])
+        ans.append(S[i:j])
+print(len(set(ans)))

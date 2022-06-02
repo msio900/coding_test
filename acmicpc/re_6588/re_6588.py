239f7a3ce62f83fd89218ca64d63690902429a4a
msio900 <msio900@gmail.com>
2022-06-02 23:02:33 +0900

    0602 1st commit

--- acmicpc/re_6588/re_6588.py
+++ acmicpc/re_6588/re_6588.py
@@ -1,10 +1,53 @@
+# import sys
+# from itertools import combinations
+#
+# input = sys.stdin.readline
+#
+# n = 1000001
+# a = [False,False] + [True]*(n-1)
+# primes=[]
+# # 에라토스테네스의 체를 이용하여 n까지의 소수를 구함
+# for i in range(2, n+1):
+#     if a[i]:
+#         primes.append(i)
+#     for j in range(2*i, n+1, i):
+#         a[j] = False
+#
+# while True :
+#     t = int(input())
+#     if t == 0:
+#         break
+#     flag = True
+#     pt = 0
+#     while True:
+#         if primes[pt] >= t:
+#             break
+#         pt += 1
+#     p_list = primes[:pt]
+#     for i in p_list:
+#         if t - i
+#         print()
+#
+#
+#
+#     if flag:
+#         print("Goldbach's conjecture is wrong.")
 import sys
 
+input = sys.stdin.readline
+
+chk = [True for i in range(1000001)]
+
+for i in range(2,1001):
+    if chk[i]:
+        for j in range(i*2, 1000001, i):
+            chk[j] = False
+
 while True:
-    n = int(sys.stdin.readline())
-    for i in range(2,n):
-        if
-
-
-    if not n:
+    n = int(input())
+    if n == 0 :
         break
+    for i in range(3, len(chk)):
+        if chk[i] and chk[n-i]:
+            print(n, "=", i, "+", n - i)
+            break
--- team.py
+++ team.py
@@ -5,11 +5,11 @@ import random
 # 채희, 승희, 지은, 우성
 
 # 오늘 스터디 참석자
-l = ["동윤", "민성", "민진", "주희", "민희", "좌홍", "청하"]
+l = ["동윤", "민성", "민진", "민희", "정화", "좌홍", "주희", "청하"]
 l = random.sample(l, len(l))
 
 # 4명당 1조로 편성
-T = int(len(l)/3)
+T = int(len(l)/4)
 team = []
 for i, name in enumerate(l):
     team.append((i%T, name))

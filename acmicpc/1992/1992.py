08a5fdedc87a9b993b665950124450a8fe0cbc39
msio900 <msio900@gmail.com>
2022-06-03 09:48:26 +0900

    0603 3rd commit

--- acmicpc/1992/1992.py
+++ acmicpc/1992/1992.py
@@ -1,66 +1,24 @@
-import sys
-from collections import deque
+n = int(input())
 
-input = sys.stdin.readline
-r, c = map(int, input().split())
-graph = []
+tree = [list(map(int,(input()))) for _ in range(n)]
+print(tree)
+result = []
 
-q_j = deque()
-q_f = deque()
+def quad_tree(x,y,n):
+    global result
+    color = tree[x][y]
 
-visited_j = [[0] * c for _ in range(r)]
-visited_f = [[0] * c for _ in range(r)]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if color != tree[i][j]: # 범위안에 한개라도 다른경우는 4분면으로 나눠서 다시 검색
+                result.append("(") # 4분면으로 나눌때 괄호를 친다.
+                quad_tree(x,y,n//2)
+                quad_tree(x, y+n//2, n//2)
+                quad_tree(x+n//2, y, n//2)
+                quad_tree(x+n//2, y+n//2, n//2)
+                result.append(")")
+                return
+    result.append(color) # 재귀로 안들어가고 for문이 전부 다 끝난 상태이기 때문에 범위안에 모든수가 같다고 볼 수 있다.
 
-move = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1]
-]
-
-for i in range(r):
-    temp = list(input())
-
-    for j in range(len(temp)):
-        if temp[j] == "J":
-            q_j.append((i, j))
-            visited_j[i][j] = 1
-
-        elif temp[j] == "F":
-            q_f.append((i, j))
-            visited_f[i][j] = 1
-
-    graph.append(temp)
-
-
-def bfs():
-    while q_f:
-        x, y = q_f.popleft()
-
-        for i in range(4):
-            nx, ny = x + move[i][0], y + move[i][1]
-
-            if 0 <= nx < r and 0 <= ny < c:
-                if not visited_f[nx][ny] and graph[nx][ny] != "#":
-                    visited_f[nx][ny] = visited_f[x][y] + 1
-                    q_f.append((nx, ny))
-
-    while q_j:
-        x, y = q_j.popleft()
-
-        for i in range(4):
-            nx, ny = x + move[i][0], y + move[i][1]
-
-            if 0 <= nx < r and 0 <= ny < c:
-                if graph[nx][ny] != "#" and not visited_j[nx][ny]:
-                    if not visited_f[nx][ny] or visited_f[nx][ny] > visited_j[x][y] + 1:
-                        visited_j[nx][ny] = visited_j[x][y] + 1
-                        q_j.append((nx, ny))
-
-            else:
-                return visited_j[x][y]
-
-    return "IMPOSSIBLE"
-
-
-print(bfs())
+quad_tree(0,0,n)
+print("".join(map(str,(result))))

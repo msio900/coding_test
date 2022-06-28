b98a3aa4540c5c2c7f821e22034e4bfaaadc23c4
msio900 <msio900@gmail.com>
2022-06-29 08:49:26 +0900

    0629 1st commit

--- programmers_practice/programmers_81301.py
+++ programmers_practice/programmers_81301.py
@@ -1,19 +1,23 @@
-def solution(record):
-    id_nickname = dict()
-    for i in record:
-        if i.split()[0] == 'Enter':
-            id_nickname[i.split()[1]] = i.split()[2]
-        if i.split()[0] == 'Change':
-            id_nickname[i.split()[1]] = i.split()[2]
-
-    answer = []
-    for i in record:
-        if i.split()[0] == 'Enter':
-            answer.append(f'{id_nickname[i.split()[1]]}님이 들어왔습니다.')
-        if i.split()[0] == 'Leave':
-            answer.append(f'{id_nickname[i.split()[1]]}님이 나갔습니다.')
+def solution(s):
+    s = s.replace('zero', '0')
+    s = s.replace('one', '1')
+    s = s.replace('two', '2')
+    s = s.replace('three', '3')
+    s = s.replace('four', '4')
+    s = s.replace('five', '5')
+    s = s.replace('six', '6')
+    s = s.replace('seven', '7')
+    s = s.replace('eight', '8')
+    s = s.replace('nine', '9')
+    answer = s
     return answer
 
 if __name__ == '__main__':
-    record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-    print(solution(record))
+    s = "one4seveneight"
+    print(solution(s))
+    s = "23four5six7"
+    print(solution(s))
+    s = "2three45sixseven"
+    print(solution(s))
+    s = "123"
+    print(solution(s))

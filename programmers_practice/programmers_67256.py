68e09331ea8b2f1a97e1dcadc590f9bebc7256b1
msio900 <msio900@gmail.com>
2022-06-30 17:02:58 +0900

    0630 1st commit

--- programmers_practice/programmers_67256.py
+++ programmers_practice/programmers_67256.py
@@ -1,23 +1,29 @@
-def solution(s):
-    s = s.replace('zero', '0')
-    s = s.replace('one', '1')
-    s = s.replace('two', '2')
-    s = s.replace('three', '3')
-    s = s.replace('four', '4')
-    s = s.replace('five', '5')
-    s = s.replace('six', '6')
-    s = s.replace('seven', '7')
-    s = s.replace('eight', '8')
-    s = s.replace('nine', '9')
-    answer = s
+def solution(numbers, hand):
+    answer = ''
+    keypad = [[1,2,3],
+              [4,5,6],
+              [7,8,9],
+              ['*', 0, '#']]
+    pre_L = 0
+    pre_R = 0
+    print(keypad[0].index(1))
+    for i in numbers:
+        if i in [1, 4, 7]:
+            answer += 'L'
+            pre_L = i
+        if i in [3, 6, 9]:
+            answer += 'R'
+            pre_R = i
+        # if i in [2, 5, 8, 0]:
+
+
+
     return answer
 
 if __name__ == '__main__':
-    s = "one4seveneight"
-    print(solution(s))
-    s = "23four5six7"
-    print(solution(s))
-    s = "2three45sixseven"
-    print(solution(s))
-    s = "123"
-    print(solution(s))
+    numbers, hand = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],	"right"
+    print(solution(numbers, hand))
+    numbers, hand = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2],	"left"
+    print(solution(numbers, hand))
+    numbers, hand = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],	"right"
+    print(solution(numbers, hand))

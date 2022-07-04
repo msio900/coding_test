231d379e3d8779a039ccfb55e38c32089ba67d79
msio900 <msio900@gmail.com>
2022-07-04 21:43:27 +0900

    0704 1st commit

--- programmers_practice/programmers_42748.py
+++ programmers_practice/programmers_42748.py
@@ -1,33 +1,12 @@
-def solution(board, moves):
-    n = len(board)
-    for i in board:
-        print(i)
-    doll_arr = []
-    ans_arr = []
-    for i in moves:
-        depth = 0
-        while True:
-            if board[len(board)-1][i - 1] == 0:
-                break
-            doll = board[depth][i - 1]
-            if doll != 0:
-                doll_arr.append(board[depth][i - 1])
-                board[depth][i - 1] = 0
-                break
-            if len(doll_arr) > 2:
-                print(doll_arr)
-                while doll_arr[-1] != doll_arr[-2]:
-                    print(doll_arr)
-                    ans_arr.append(doll_arr.pop())
-                    ans_arr.append(doll_arr.pop())
-            depth +=1
-    print('변화 후')
-    for i in board:
-        print(i)
-    print(ans_arr)
-    answer = len(ans_arr)
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        num_arr = array[i - 1:j]
+        num_arr.sort()
+        answer.append(num_arr[k-1])
     return answer
 
 if __name__ == '__main__':
-    board, moves = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4]
-    print(solution(board, moves))
+    array, commands = [1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+    print(solution(array, commands))

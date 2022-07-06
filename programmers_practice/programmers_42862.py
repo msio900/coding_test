7b692efd5dbef4a68930e2a84c7ff65e53e9cf25
msio900 <msio900@gmail.com>
2022-07-06 10:00:39 +0900

    0706 1st commit

--- programmers_practice/programmers_42862.py
+++ programmers_practice/programmers_42862.py
@@ -1,15 +1,19 @@
 def solution(n, lost, reserve):
-    students = [i for i in range(1, n+1) if i not in lost]
+    answer = n
 
-    for i in lost:
-        if i in reserve:
-            students.append(reserve.pop(reserve.index(i)))
-        elif (i - 1) in reserve:
-            students.append(reserve.pop(reserve.index(i - 1)))
-        elif (i + 1) in reserve:
-            students.append(reserve.pop(reserve.index(i + 1)))
+    new_lost = [l for l in lost if l not in reserve]
+    new_lost.sort()
+    new_reserve = [l for l in reserve if l not in lost]
+    new_reserve.sort()
 
-    answer = len(students)
+    for l in new_lost:
+        if l - 1 in new_reserve:
+            new_reserve.remove(l - 1)
+        elif l + 1 in new_reserve:
+            new_reserve.remove(l + 1)
+        else:
+            answer -= 1
+
     return answer
 
 if __name__ == '__main__':

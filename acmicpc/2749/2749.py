f55d2577281439c6e18f46d9fd56c4e19df0a0fd
msio900 <msio900@gmail.com>
2022-07-08 09:49:10 +0900

    0708 2nd commit

---
+++ acmicpc/2749/2749.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+mod = 1000000
+fibo = [0, 1]
+p = mod//10*15
+
+for i in range(2,p):
+    fibo.append(fibo[i-1]+fibo[i-2])
+    fibo[i] %= mod
+
+print(fibo[n%p])

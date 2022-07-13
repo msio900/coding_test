f3c3c2f6acc6778a46b5fe07313dac347a880560
msio900 <msio900@gmail.com>
2022-07-13 09:34:32 +0900

    0713 2nd commit

--- acmicpc/1837/1837.py
+++ acmicpc/1837/1837.py
@@ -2,10 +2,29 @@ import sys
 
 input = sys.stdin.readline
 
+# 에라토스테네스의 체
+n = 10100
+a = [False,False] + [True]*(n-1)
+primes = []
 
-n = int(input())
-a = list(map(int, input().split()))
-b, c = map(int, input().split())
+for i in range(2, n+1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2*i, n+1, i):
+            a[j] = False
 
-print(a, b, c)
+# 비밀키 pq : 특정 소수 p와 q의 곱
+
+p, k = map(int, input().split())
+
+for i in primes:
+    if p % i == 0:
+        num = i
+        break
+
+if num <= k:
+    print('BAD', num)
+else:
+    print('GOOD')
+
 

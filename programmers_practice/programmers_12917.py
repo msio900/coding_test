d5c6f13f188b481a85ac275c819035355128ccb7
msio900 <msio900@gmail.com>
2022-07-15 09:09:48 +0900

    0715 1st commit

--- programmers_practice/programmers_12917.py
+++ programmers_practice/programmers_12917.py
@@ -1,21 +1,9 @@
-def solution(a, b):
-    answer = 0
-    if a > b:
-        for i in range(b, a + 1):
-            answer += i
-    elif a == b:
-        answer = a
-    else:
-        for i in range(a, b+1):
-            answer += i
+def solution(s):
+    answer = ''.join(sorted(s, reverse=True))
     return answer
 
 if __name__ == '__main__':
-    a, b = 3,	5
-    print(solution(a, b))
-    a, b = 3,	3
-    print(solution(a, b))
-    a, b = 5,	3
-    print(solution(a, b))
+    s = "Zbcdefg"
+    print(solution(s))
 
 

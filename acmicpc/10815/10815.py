d91649c96f8192c4d59273d9318b6b31e4b0d271
msio900 <msio900@gmail.com>
2022-07-19 09:29:19 +0900

    0719 2nd commit

--- acmicpc/10815/10815.py
+++ acmicpc/10815/10815.py
@@ -1,16 +1,27 @@
 import sys
 
-alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
-nums = [0]*len(alphabet)
+input = sys.stdin.readline
 
+N = int(input())
+cards = list(map(int, input().split()))
+M = int(input())
+nums = list(map(int, input().split()))
 
-s = sys.stdin.read()
-# print(s)
-for i in s:
-    if i in alphabet:
-        nums[ord(i) - ord('a')] += 1
-answer = []
-for k, v in enumerate(nums):
-    if v == max(nums):
-        answer.append(alphabet[k])
-print(''.join(answer))
+cards.sort()
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in range(M):
+    if binary_search(cards, nums[i], 0, N -1) is not None:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')

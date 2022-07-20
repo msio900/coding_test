144955ede23f401491f86a9d83dbedfec7bf4ae1
msio900 <msio900@gmail.com>
2022-07-20 09:52:45 +0900

    0720 2nd commit

--- acmicpc/1920/1920.py
+++ acmicpc/1920/1920.py
@@ -2,12 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-cards = list(map(int, input().split()))
-M = int(input())
+n = int(input())
+integer = list(map(int, input().split()))
+m = int(input())
 nums = list(map(int, input().split()))
 
-cards.sort()
+integer.sort()
 
 def binary_search(array, target, start, end):
     while start <= end:
@@ -20,8 +20,9 @@ def binary_search(array, target, start, end):
             start = mid + 1
     return None
 
-for i in range(M):
-    if binary_search(cards, nums[i], 0, N -1) is not None:
+
+for i in range(m):
+    if binary_search(integer, nums[i], 0, n - 1) is not None:
         print(1, end=' ')
     else:
         print(0, end=' ')

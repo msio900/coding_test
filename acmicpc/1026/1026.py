c39eeaf4ca1760fd327b53d975222aebd56edb18
msio900 <msio900@gmail.com>
2022-07-21 08:57:13 +0900

    0721 2nd commit

--- acmicpc/1026/1026.py
+++ acmicpc/1026/1026.py
@@ -3,26 +3,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-integer = list(map(int, input().split()))
-m = int(input())
-nums = list(map(int, input().split()))
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
 
-integer.sort()
-
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
-
-
-for i in range(m):
-    if binary_search(integer, nums[i], 0, n - 1) is not None:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+a_arr.sort()
+b_arr.sort(reverse=True)
+s = 0
+for i in range(n):
+    s+= a_arr[i]*b_arr[i]
+print(s)

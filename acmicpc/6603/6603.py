7ff164569a3596b0b572aa1f86128f960dacb47d
msio900 <msio900@gmail.com>
2022-08-03 21:23:44 +0900

    0803 2nd commit

--- acmicpc/6603/6603.py
+++ acmicpc/6603/6603.py
@@ -1,10 +1,15 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
-t = int(input())
-
-for _ in range(t):
-    n, m = map(int, input().split())
-    doc = list(map(int, input().split()))
-    dict = dict
+while True: # break가 나올 때까지
+    l = list(map(int, input().split()))
+    if l[0] == 0: # 반복문 break조건
+        break
+    else:
+        s = l[1:]
+        for i in combinations(s, 6): # 주어진 수 중 6개를 뽑는 조합
+            i = list(i)
+            print(*i)
+        print( )

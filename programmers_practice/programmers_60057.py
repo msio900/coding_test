7655d4e04b813fdcd67480d6b2e90c957dde161e
msio900 <msio900@gmail.com>
2022-08-15 22:00:16 +0900

    0815 1st commit

--- programmers_practice/programmers_60057.py
+++ programmers_practice/programmers_60057.py
@@ -1,6 +1,35 @@
-a, b = map(int, input().split())
-for i in range(b):
-    print('*' * a)
+def solution(s):
+    ans=1e9
+    if len(s) < 3:
+        return len(s)
+    for i in range(1,len(s)//2+1):
+        cnt=1
+        strr=""
+        pre=s[0:i]
+        for j in range(i,len(s)+i,i):
+            if pre == s[j:j+i]:
+                cnt+=1
+            else:
+                if cnt !=1:
+                    strr+=str(cnt)+pre
+                else:
+                    strr+=pre
+                cnt=1
+                pre=s[j:j+i]
+        ans=min(ans,len(strr))
+    return ans
+
+if __name__ =='__main__':
+    s = "aabbaccc"
+    print(solution(s))
+    s = "ababcdcdababcdcd"
+    print(solution(s))
+    s = "abcabcdede"
+    print(solution(s))
+    s = "abcabcabcabcdededededede"
+    print(solution(s))
+    s = "xababcdcdababcdcd"
+    print(solution(s))
 
 
 

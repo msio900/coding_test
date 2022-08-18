282d33564505489ef2354635ea1e8a122a76bff3
msio900 <msio900@gmail.com>
2022-08-18 09:41:49 +0900

    0818 1st commit

--- programmers_practice/programmers_43165.py
+++ programmers_practice/programmers_43165.py
@@ -1,35 +1,22 @@
-def solution(s):
-    answer =1e9
-    if len(s) < 3:
-        return len(s)
-    for i in range(1,len(s)//2+1):
-        cnt=1
-        strr=""
-        pre=s[0:i]
-        for j in range(i,len(s)+i,i):
-            if pre == s[j:j+i]:
-                cnt+=1
-            else:
-                if cnt !=1:
-                    strr+=str(cnt)+pre
-                else:
-                    strr+=pre
-                cnt=1
-                pre=s[j:j+i]
-        answer = min(ans,len(strr))
+from itertools import product
+
+def solution(numbers, target):
+    answer = 0
+    num_list = []
+    for i in numbers:
+        num_list.append([i, -i])
+    num_lists = list(product(*num_list))
+    for i in num_lists:
+        if sum(i) == target:
+            answer += 1
     return answer
 
 if __name__ =='__main__':
-    s = "aabbaccc"
-    print(solution(s))
-    s = "ababcdcdababcdcd"
-    print(solution(s))
-    s = "abcabcdede"
-    print(solution(s))
-    s = "abcabcabcabcdededededede"
-    print(solution(s))
-    s = "xababcdcdababcdcd"
-    print(solution(s))
+    numbers, target = [1, 1, 1, 1, 1], 3
+    print(solution(numbers, target))
+    numbers, target = [4, 1, 2, 1], 4
+    print(solution(numbers, target))
+
 
 
 

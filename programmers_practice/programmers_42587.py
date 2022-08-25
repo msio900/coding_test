d4eb7dabc23094a9767eaeaa9bffe4a90ea79af2
msio900 <msio900@gmail.com>
2022-08-26 08:45:28 +0900

    0826 1st commit

---
+++ programmers_practice/programmers_42587.py
@@ -0,0 +1,14 @@
+def solution(priorities, location):
+	answer = 0
+    return answer
+
+if __name__ =='__main__':
+    priorities,	location = [2, 1, 3, 2], 2
+    print(solution(priorities, location))
+    priorities,	location = [1, 1, 9, 1, 1, 1], 0
+    print(solution(priorities, location))
+
+
+
+
+

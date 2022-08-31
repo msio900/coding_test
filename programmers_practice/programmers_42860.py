6c7124c3d091e047b8f8992f108f664d054cf067
msio900 <msio900@gmail.com>
2022-08-31 21:07:22 +0900

    0831 1st commit

--- programmers_practice/programmers_42860.py
+++ programmers_practice/programmers_42860.py
@@ -1,32 +1,8 @@
 def solution(name):
     answer = 0
     alpha = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-    # for i in name:
-    #     answer += 1
-    #     if i =='A':
-    #         pass
-    #     elif temp < alpha.index(i):
-    #         print('▼', alpha[temp:alpha.index(i)], len(alpha[temp:alpha.index(i)]))
-    #         print('▲', alpha[:temp]+alpha[alpha.index(i):], len(alpha[:temp]+alpha[alpha.index(i):]))
-    #         print(min([len(alpha[temp:alpha.index(i)]), len(alpha[:temp] + alpha[alpha.index(i):])]))
-    #         answer += min([len(alpha[temp:alpha.index(i)]), len(alpha[:temp]+alpha[alpha.index(i):])])
-    #     elif temp > alpha.index(i):
-    #         print('▼', alpha[alpha.index(i):temp], len(alpha[alpha.index(i):temp]))
-    #         print('▲', alpha[:alpha.index(i)] + alpha[temp:], len(alpha[:alpha.index(i)] + alpha[temp:]))
-    #         print(min([len(alpha[alpha.index(i):temp]), len(alpha[:alpha.index(i)] + alpha[temp:])]))
-    #         answer += min([len(alpha[alpha.index(i):temp]), len(alpha[:alpha.index(i)] + alpha[temp:])])
-    #     temp = alpha.index(i)
-    for i in name:
-        answer += min([len(alpha[alpha.index(i):]), len(alpha[:alpha.index(i)])])
-        # if alpha.index(i) > 12:
-        #     print(alpha[alpha.index(i):], len(alpha[alpha.index(i):]))
-        #     answer += len(alpha[alpha.index(i):])
-        # elif alpha.index(i) < 12:
-        #     print(alpha[:alpha.index(i)],len(alpha[:alpha.index(i)]))
-        #     answer += len(alpha[:alpha.index(i)])
-        # elif i == 'N':
-        #     answer += 11
-    answer += len(name) - 1
+    for i in range(len(name)):
+        answer += min([len(alpha[alpha.index(name[i]):]), len(alpha[:alpha.index(name[i])])])
 
     return answer
 

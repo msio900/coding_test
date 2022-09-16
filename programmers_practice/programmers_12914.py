02ce51bd21915ca39a331e45e8124d07e7a2d5df
msio900 <msio900@gmail.com>
2022-09-16 09:06:11 +0900

    0916 1st commit

--- programmers_practice/programmers_12914.py
+++ programmers_practice/programmers_12914.py
@@ -1,6 +1,13 @@
+from collections import defaultdict
+
 def solution(n):
-    answer = 0
-    return answer
+    d = defaultdict(int)
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n+2):
+        d[i] = (d[i-1]+d[i-2])%1234567
+
+    return d[n]
 
 if __name__ == '__main__':
     n = 4

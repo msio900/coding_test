00b5e454f51cfb27df8733eca4b100a5d43ec415
msio900 <msio900@gmail.com>
2022-10-22 20:46:53 +0900

    1022 1st commit

--- programmers_practice/programmers_42890.py
+++ programmers_practice/programmers_42890.py
@@ -1,9 +1,31 @@
+from itertools import combinations
+
 def solution(relation):
-    answer = 0
-    return answer
+    x, y = len(relation[0]), len(relation)
+    col = list(range(x))
+    cases = []
+    uniques = []
+    for i in range(1, x + 1):
+        case = list(combinations(col, i))
+        cases += case
+    for case in cases:
+        # print(case)
+        keys = []
+        for j in range(y):
+            key = ''
+            for i in case:
+                key += relation[j][i]
+            keys.append(key)
+        if len(keys) == len(set(keys)):
+            for x in uniques:
+                if set(x).issubset(set(case)):
+                    break
+            else:
+                uniques.append(list(case))
+
+    return len(uniques)
+
 
 if __name__ == '__main__':
-    N, stages = 5, [2, 1, 2, 6, 2, 4, 3, 3]
-    print(solution(N, stages))
-    N, stages = 4, [4,4,4,4,4]
-    print(solution(N, stages))
+    relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
+    print(f'답은 {solution(relation)}')

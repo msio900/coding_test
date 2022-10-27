4fcc90fa0faff8aaa6de60ab2ba9940ae84254be
msio900 <msio900@gmail.com>
2022-10-27 17:52:11 +0900

    1027 1st commit

--- this_is_codingtest/5_DFS_BFS_problem_1.py
+++ this_is_codingtest/5_DFS_BFS_problem_1.py
@@ -1,34 +1,81 @@
-# N, M을 공백을 기준으로 구분하여 입력 받기
-n, m = map(int, input().split())
+# # N, M을 공백을 기준으로 구분하여 입력 받기
+# n, m = map(int, input().split())
+#
+# # 2차원 리스트의 맵 정보 입력 받기
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+#
+# # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+# def dfs(x, y):
+#     # 주어진 범위를 벗어나는 경우에는 즉시 종료
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+#     # 현재 노드를 아직 방문하지 않았다면
+#     if graph[x][y] == 0:
+#         # 해당 노드 방문 처리
+#         graph[x][y] = 1
+#         # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+#         dfs(x - 1, y)
+#         dfs(x, y - 1)
+#         dfs(x + 1, y)
+#         dfs(x, y + 1)
+#         return True
+#     return False
+#
+# # 모든 노드(위치)에 대하여 음료수 채우기
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         # 현재 위치에서 DFS 수행
+#         if dfs(i, j) == True:
+#             result += 1
+#
+# print(result) # 정답 출력
 
-# 2차원 리스트의 맵 정보 입력 받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
 
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+
+
+n, m = 15, 14
+graph = '''
+00000111100000
+11111101111110
+11011101101110
+11011101100000
+11011111111111
+11011111111100
+11000000011111
+01111111111111
+00000000011111
+01111111111000
+00011111111000
+00000001111000
+11111111110011
+11100011111111
+11100011111111
+'''
+
+
+def DFS(x, y):
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-    # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
-        # 해당 노드 방문 처리
         graph[x][y] = 1
-        # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
-        dfs(x - 1, y)
-        dfs(x, y - 1)
-        dfs(x + 1, y)
-        dfs(x, y + 1)
+        DFS(x - 1, y)
+        DFS(x, y - 1)
+        DFS(x + 1, y)
+        DFS(x, y + 1)
         return True
     return False
+answer = 0
 
-# 모든 노드(위치)에 대하여 음료수 채우기
-result = 0
-for i in range(n):
-    for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            result += 1
+graph = graph.split()
+for i in graph:
+    print(list(map(int, i)))
 
-print(result) # 정답 출력
+# for i in range(n):
+#     for j in range(m):
+#         if DFS(i, j) == True:
+#             answer += 1
+
+

e22492fccaaa0f1e3638eef5b4183db8d53a62ac
msio900 <msio900@gmail.com>
2022-11-01 17:29:22 +0900

    1101 1st commit

--- acmicpc/15666/15666.py
+++ acmicpc/15666/15666.py
@@ -1,13 +1,28 @@
-import sys
-from itertools import combinations_with_replacement
+# import sys
+# from itertools import combinations_with_replacement
+#
+# input = sys.stdin.readline
+#
+# n, m = map(int, input().split())
+#
+# n_list = list(map(int, input().split()))
+#
+# n_list = sorted(list(set(n_list)))
+#
+# for i in combinations_with_replacement(n_list, m):
+#     print(*i)
 
-input = sys.stdin.readline
+def backTracking(seq, cnt):
+    cnt -= 1
+    if cnt == -1:
+        print(*seq)
+        return
+    for i in arr:
+        if not seq or i >= seq[-1]:
+            backTracking(seq+[i], cnt)
 
-n, m = map(int, input().split())
+n, m = 4, 2
+arr = sorted(list(set([9,7,9,1])))
 
-n_list = list(map(int, input().split()))
+print(backTracking([], m))
 
-n_list = sorted(list(set(n_list)))
-
-for i in combinations_with_replacement(n_list, m):
-    print(*i)
